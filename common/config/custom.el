7668e83e84807db860d73848503fe837e1e3738f
Magnus Malm <magnusmalm@gmail.com>
2018-06-13 01:58:25 +0200

    general: Add *.d to ignore list for completion in general.
    Signed-off-by: Magnus Malm <magnusmalm@gmail.com>

--- common/config/custom.el
+++ common/config/custom.el
@@ -11,6 +11,11 @@
  '(company-quickhelp-max-lines nil)
  '(company-quickhelp-use-propertized-text t)
  '(company-selection-wrap-around t)
+ '(completion-ignored-extensions
+   '(".d" ".o" "~" ".bin" ".lbin" ".so" ".a" ".ln" ".blg" ".bbl" ".elc" ".lof" ".glo" ".idx" ".lot" ".svn/" ".hg/" ".git/" ".bzr/" "CVS/" "_darcs/"
+     "_MTN/" ".fmt" ".tfm" ".class" ".fas" ".lib" ".mem" ".x86f" ".sparcf" ".dfsl" ".pfsl" ".d64fsl" ".p64fsl" ".lx64fsl" ".lx32fsl" ".dx64fsl"
+     ".dx32fsl" ".fx64fsl" ".fx32fsl" ".sx64fsl" ".sx32fsl" ".wx64fsl" ".wx32fsl" ".fasl" ".ufsl" ".fsl" ".dxl" ".lo" ".la" ".gmo" ".mo" ".toc" ".aux"
+     ".cp" ".fn" ".ky" ".pg" ".tp" ".vr" ".cps" ".fns" ".kys" ".pgs" ".tps" ".vrs" ".pyc" ".pyo"))
  '(erc-auto-query 'window-noselect)
  '(erc-modules
    '(fill move-to-prompt stamp spelling hl-nicks netsplit fill button match track completion readonly networks ring autojoin noncommands irccontrols move-to-prompt stamp menu list))
