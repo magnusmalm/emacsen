2bd370a30eac3e5cb10ffff4c61ffa861faad042
Magnus Malm <magnusmalm@gmail.com>
2018-06-13 02:01:22 +0200

    echo: Increase max height if echo area to 50% of frame height.
    Signed-off-by: Magnus Malm <magnusmalm@gmail.com>

--- common/config/master.el
+++ common/config/master.el
@@ -1039,6 +1039,7 @@ monopolizing the minibuffer."
 
 (setf enable-recursive-minibuffers t)
 (minibuffer-depth-indicate-mode)
+(setf max-mini-window-height 0.5)
 
 (setf nsm-settings-file
       (expand-file-name "var/network-security.data/" user-emacs-directory))
