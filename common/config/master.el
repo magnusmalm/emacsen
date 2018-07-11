437636ceb982bce200ec982ee81f50ca977180a9
Magnus Malm <magnusmalm@gmail.com>
2018-07-12 01:52:10 +0200

    modeline lighters: Hide all but a select few with rich-minority.
    Signed-off-by: Magnus Malm <magnusmalm@gmail.com>

--- common/config/master.el
+++ common/config/master.el
@@ -2014,6 +2014,18 @@ If ABSOLUTE is non-nil, text scale is applied relative to the default font size
 (use-package smart-mode-line-powerline-theme)
 
 (use-package rich-minority
+  :straight (:host github
+		   :repo "malabarba/rich-minority"
+		   :branch "master")
+  :init
+  (setq rm-whitelist
+	(format "^ \\(%s\\)$"
+		(mapconcat #'identity
+                           '("Fly.*" "Proj.*")
+                           "\\|")))
+  :config
+  (rich-minority-mode 1))
+
 (use-package fill-column-indicator
   :init
   (add-hook 'prog-mode-hook #'fci-mode)
