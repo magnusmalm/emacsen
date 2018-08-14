5bccb6e0d9f7bc7a9436dbfac4b74e7183257a5a
Magnus Malm <magnusmalm@gmail.com>
2018-08-14 07:51:57 +0200

    Mail and Org stuff loaded in the Org server profile.
    Signed-off-by: Magnus Malm <magnusmalm@gmail.com>

--- common/config/master.el
+++ common/config/master.el
@@ -2280,166 +2280,6 @@ If ABSOLUTE is non-nil, text scale is applied relative to the default font size
 
 (use-package latex-preview-pane)
 
-(if (server-is "mail")
-    (progn
-      (message "Loading MAIL stuff...")
-
-      (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
-
-      (require 'mu4e)
-      (require 'smtpmail)
-      (require 'gnus-dired)
-
-      (load-file "~/.secrets/emacs-mail.el")
-
-      (setf mu4e-maildir "~/.mail")
-
-      ;; Directory to save attachments
-      (setf mu4e-attachment-dir "~/attachments/")
-      ;; List of mail accounts.
-      (defvar my-mu4e-account-alist nil)
-      ;; List of email signatures that can be added to a mail.
-      (defvar mu4e-mail-sigs nil)
-      ;; Shortcuts to often visited mailboxes
-      (setf mu4e-maildir-shortcuts nil)
-      ;; Command used to get mail (offlineimap, mbsync, etc)
-      (setf mu4e-get-mail-command nil)
-      ;; If non-nil _and_ mu4e is running, get mail in the background periodically
-      ;; Value in minutes
-      (setf mu4e-update-interval nil)
-
-
-      (setf mu4e-use-fancy-chars t)
-      (setf mu4e-headers-date-format "%Y-%m-%d %H:%M")
-      (setf mu4e-headers-skip-duplicates t)
-      (setf mu4e-headers-include-related t)
-      (setf mu4e-view-date-format "%a %Y-%m-%d %H:%M")
-      (setf gnus-dired-mail-mode 'mu4e-user-agent)
-      (setf mu4e-change-filenames-when-moving t)
-      ;; show full addresses in view message (instead of just names)
-      ;; toggle per name with M-RET
-      (setf mu4e-view-show-addresses t)
-
-      (setf message-send-mail-function 'smtpmail-send-it)
-      (setf starttls-use-gnutls t)
-      (setf smtpmail-debug-info t)
-
-      ;; when you want to use some external command for html->text
-      ;; conversion, e.g. the  html2text  program
-      ;; (cpbotha: html2text sees to work better than the built-in one)
-      (setf mu4e-html2text-command "html2text")
-
-      ;; mu4e-action-view-in-browser is built into mu4e
-      ;; by adding it to these lists of custom actions
-      ;; it can be invoked by first pressing a, then selecting
-      (add-to-list 'mu4e-headers-actions
-		   '("in browser" . mu4e-action-view-in-browser) t)
-      (add-to-list 'mu4e-view-actions
-		   '("in browser" . mu4e-action-view-in-browser) t)
-
-      ;; the headers to show in the headers list   a pair of a field
-      ;; and its width, with `nil  meaning  unlimited
-      ;; (better only use that for the last field.
-      ;; These are the defaults:
-      (setf mu4e-headers-fields
-	    '( (:date . 20)
-	       (:maildir . 20)
-	       (:flags . 6)
-	       (:from-or-to . 22)
-	       (:subject . nil)))
-
-      (use-package mu4e-maildirs-extension
-	:ensure t)
-
-      (mu4e-maildirs-extension)
-
-      ;; enable inline images
-      (setf mu4e-view-show-images t)
-
-      ;; use imagemagick, if available
-      (when (fboundp 'imagemagick-register-types)
-	(imagemagick-register-types))
-
-      (add-hook 'mu4e-compose-mode-hook
-		(defun my-do-compose-stuff ()
-		  "My settings for message composition."
-		  (set-fill-column 72)
-		  (flyspell-mode)))
-
-      (setf mu4e-compose-signature-auto-include nil)
-
-      (defun my-render-html-message ()
-	(let ((dom (libxml-parse-html-region (point-min) (point-max))))
-	  (erase-buffer)
-	  (shr-insert-document dom)
-	  (goto-char (point-min))))
-
-      (setf mu4e-html2text-command 'my-render-html-message)
-      ;; make the `gnus-dired-mail-buffers' function also work on
-      ;; message-mode derived modes, such as mu4e-compose-mode
-      (defun gnus-dired-mail-buffers ()
-	"Return a list of active message buffers."
-	(let (buffers)
-	  (save-current-buffer
-	    (dolist (buffer (buffer-list t))
-	      (set-buffer buffer)
-	      (when (and (derived-mode-p 'message-mode)
-			 (null message-sent-message-via))
-		(push (buffer-name buffer) buffers))))
-	  (nreverse buffers)))
-      (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
-
-      (defun my-mu4e-set-account ()
-	"Set the account for composing a message."
-	(let* ((account
-		(if mu4e-compose-parent-message
-		    (let ((maildir (mu4e-message-field mu4e-compose-parent-message :maildir)))
-		      (string-match "/\\(.*?\\)/" maildir)
-		      (match-string 1 maildir))
-		  (completing-read (format "Compose with account: (%s) "
-					   (mapconcat #'(lambda (var) (car var))
-						      my-mu4e-account-alist "/"))
-				   (mapcar #'(lambda (var) (car var)) my-mu4e-account-alist)
-				   nil t nil nil (caar my-mu4e-account-alist))))
-	       (account-vars (cdr (assoc account my-mu4e-account-alist))))
-	  (message "Account set to %s" account)
-	  (if account-vars
-	      (mapc #'(lambda (var)
-			(set (car var) (cadr var)))
-		    account-vars)
-	    (error "No email account found"))))
-      (add-hook 'mu4e-compose-pre-hook 'my-mu4e-set-account)
-
-      (defun my-mu4e-choose-signature ()
-	"Insert one of a number of sigs"
-	(interactive)
-	(let ((message-signature
-	       (mu4e-read-option "Signature:"
-				 mu4e-mail-sigs)))
-	  (message-insert-signature)))
-      (add-hook 'mu4e-compose-mode-hook
-		(lambda () (local-set-key (kbd "C-c C-w") #'my-mu4e-choose-signature)))
-
-      ;; works only for emacs with xwidget support
-      (defun my-mu4e-action-view-with-xwidget (msg)
-	"View the body of the message inside xwidget-webkit."
-	(unless (fboundp 'xwidget-webkit-browse-url)
-	  (mu4e-error "No xwidget support available"))
-	(let* ((html (mu4e-message-field msg :body-html))
-	       (txt (mu4e-message-field msg :body-txt))
-	       (tmpfile (format "%s%x.html" temporary-file-directory (random t))))
-	  (unless (or html txt)
-	    (mu4e-error "No body part for this message"))
-	  (with-temp-buffer
-	    ;; simplistic -- but note that it's only an example...
-	    (insert (or html (concat "<pre>" txt "</pre>")))
-	    (write-file tmpfile)
-	    (xwidget-webkit-browse-url (concat "file://" tmpfile) t))))
-
-      (add-to-list 'mu4e-view-actions
-		   '("xViewXWidget" . my-mu4e-action-view-with-xwidget) t)
-      (message "Loading MAIL stuff done!")))
-
 (if (server-is "irc")
     (progn
       (message "Loading IRC stuff...")
@@ -3384,6 +3224,161 @@ TAG is chosen interactively from the global tags completion table."
       (add-hook 'org-finalize-agenda-hook #'place-agenda-tags)
 
       (setf org-tags-column 0)))
+      (message "Loading MAIL stuff...")
+
+      (add-to-list 'load-path "/usr/share/emacs/site-lisp/mu4e")
+
+      (require 'mu4e)
+      (require 'smtpmail)
+      (require 'gnus-dired)
+      (require'org-mu4e)
+      (setq org-mu4e-link-query-in-headers-mode nil)
+
+      (setf mu4e-maildir "~/.mail")
+
+      (load-file "~/.secrets/emacs-mail.el")
+
+      ;; Directory to save attachments
+      (setf mu4e-attachment-dir "~/attachments/")
+      ;; List of mail accounts.
+      (defvar my-mu4e-account-alist nil)
+      ;; List of email signatures that can be added to a mail.
+      (defvar mu4e-mail-sigs nil)
+      ;; Shortcuts to often visited mailboxes
+
+      (setq mu4e-maildir-shortcuts
+  	    '( ("/gmail/archive" . ?A)
+  	       ("/gmail/drafts" . ?D)
+	       ("/gmail/inbox" . ?I)
+  	       ("/gmail/sent" . ?S)
+	       ("/gmail/trash" . ?T)
+
+	       ("/work/drafts" . ?d)
+	       ("/work/inbox"  . ?i)
+  	       ("/work/sent" . ?s)
+	       ("/work/trash" . ?t)))
+
+      (setf mu4e-compose-complete-only-personal t)
+      ;; If non-nil _and_ mu4e is running, get mail in the background periodically
+      ;; Value in minutes
+      (setf mu4e-update-interval nil)
+
+      (setf mu4e-use-fancy-chars t)
+      (setf mu4e-headers-date-format "%Y-%m-%d %H:%M")
+      (setf mu4e-headers-skip-duplicates t)
+      (setf mu4e-headers-include-related t)
+      (setf mu4e-view-date-format "%a %Y-%m-%d %H:%M")
+      (setf gnus-dired-mail-mode 'mu4e-user-agent)
+      (setf mu4e-change-filenames-when-moving t)
+      ;; show full addresses in view message (instead of just names)
+      ;; toggle per name with M-RET
+      (setf mu4e-view-show-addresses t)
+
+      (setf message-send-mail-function 'smtpmail-send-it)
+      (setf starttls-use-gnutls t)
+      (setf smtpmail-debug-info t)
+
+      ;; when you want to use some external command for html->text
+      ;; conversion, e.g. the  html2text  program
+      ;; (cpbotha: html2text sees to work better than the built-in one)
+      (setf mu4e-html2text-command "html2text")
+
+      ;; mu4e-action-view-in-browser is built into mu4e
+      ;; by adding it to these lists of custom actions
+      ;; it can be invoked by first pressing a, then selecting
+      (add-to-list 'mu4e-headers-actions
+		   '("in browser" . mu4e-action-view-in-browser) t)
+      (add-to-list 'mu4e-view-actions
+		   '("in browser" . mu4e-action-view-in-browser) t)
+
+      ;; the headers to show in the headers list   a pair of a field
+      ;; and its width, with `nil  meaning  unlimited
+      ;; (better only use that for the last field.
+      ;; These are the defaults:
+      (setf mu4e-headers-fields
+	    '( (:date . 20)
+	       (:maildir . 20)
+	       (:flags . 6)
+	       (:from-or-to . 35)
+	       (:subject . nil)))
+
+      ;; enable inline images
+      (setf mu4e-view-show-images t)
+
+      (setq mu4e-context-policy 'pick-first)
+
+      (setq mu4e-compose-context-policy nil)
+
+      (setq message-kill-buffer-on-exit t)
+
+      ;; use imagemagick, if available
+      (when (fboundp 'imagemagick-register-types)
+	(imagemagick-register-types))
+
+      (add-hook 'mu4e-compose-mode-hook
+		(defun my-do-compose-stuff ()
+		  "My settings for message composition."
+		  (set-fill-column 72)
+		  (super-save-mode -1)
+		  (flyspell-mode)))
+
+      (setf mu4e-compose-signature-auto-include t)
+
+      (defun my-render-html-message ()
+	(let ((dom (libxml-parse-html-region (point-min) (point-max))))
+	  (erase-buffer)
+	  (shr-insert-document dom)
+	  (goto-char (point-min))))
+
+      (setf mu4e-html2text-command 'my-render-html-message)
+      ;; make the `gnus-dired-mail-buffers' function also work on
+      ;; message-mode derived modes, such as mu4e-compose-mode
+      (defun gnus-dired-mail-buffers ()
+	"Return a list of active message buffers."
+	(let (buffers)
+	  (save-current-buffer
+	    (dolist (buffer (buffer-list t))
+	      (set-buffer buffer)
+	      (when (and (derived-mode-p 'message-mode)
+			 (null message-sent-message-via))
+		(push (buffer-name buffer) buffers))))
+	  (nreverse buffers)))
+      (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
+
+      ;; works only for emacs with xwidget support
+      (defun my-mu4e-action-view-with-xwidget (msg)
+	"View the body of the message inside xwidget-webkit."
+	(unless (fboundp 'xwidget-webkit-browse-url)
+	  (mu4e-error "No xwidget support available"))
+	(let* ((html (mu4e-message-field msg :body-html))
+	       (txt (mu4e-message-field msg :body-txt))
+	       (tmpfile (format "%s%x.html" temporary-file-directory (random t))))
+	  (unless (or html txt)
+	    (mu4e-error "No body part for this message"))
+	  (with-temp-buffer
+	    ;; simplistic -- but note that it's only an example...
+	    (insert (or html (concat "<pre>" txt "</pre>")))
+	    (write-file tmpfile)
+	    (xwidget-webkit-browse-url (concat "file://" tmpfile) t))))
+
+      (add-to-list 'mu4e-view-actions
+		   '("xViewXWidget" . my-mu4e-action-view-with-xwidget) t)
+
+      (use-package org-caldav
+	:config
+	(defun my-org-caldav-advice (orig-fun &rest args)
+	  (let ((xml-default-ns-bak xml-default-ns)
+		(org-version-bak org-version))
+	    (setq xml-default-ns '(("" . "DAV:")))
+	    (setq org-version "8.2.7")
+	    (apply orig-fun args)
+	    (setq xml-default-ns xml-default-ns-bak)
+	    (setq org-version org-version-bak)))
+	(advice-add 'org-caldav-sync :around #'my-org-caldav-advice)
+	(setf org-caldav-uuid-extension ".EML")
+	(setf org-caldav-save-directory (expand-file-name "~/sync/org/calendars/" user-emacs-directory)))
+
+      (message "Loading MAIL stuff done!")))
 
 (setf fill-column 80)
 
