6644e5d390ce667d4e279de49dca334761c7817b
Magnus Malm <magnusmalm@gmail.com>
2018-09-17 22:01:30 +0200

    Live without XWidgets in favor of LUCID X-Toolkit.
    Signed-off-by: Magnus Malm <magnusmalm@gmail.com>

--- common/config/master.el
+++ common/config/master.el
@@ -3580,24 +3580,6 @@ TAG is chosen interactively from the global tags completion table."
     (nreverse buffers)))
 (add-hook 'dired-mode-hook 'turn-on-gnus-dired-mode)
 
-;; works only for emacs with xwidget support
-(defun my-mu4e-action-view-with-xwidget (msg)
-  "View the body of the message inside xwidget-webkit."
-  (unless (fboundp 'xwidget-webkit-browse-url)
-    (mu4e-error "No xwidget support available"))
-  (let* ((html (mu4e-message-field msg :body-html))
-	 (txt (mu4e-message-field msg :body-txt))
-	 (tmpfile (format "%s%x.html" temporary-file-directory (random t))))
-    (unless (or html txt)
-      (mu4e-error "No body part for this message"))
-    (with-temp-buffer
-      ;; simplistic -- but note that it's only an example...
-      (insert (or html (concat "<pre>" txt "</pre>")))
-      (write-file tmpfile)
-      (xwidget-webkit-browse-url (concat "file://" tmpfile) t))))
-
-(add-to-list 'mu4e-view-actions
-	     '("xViewXWidget" . my-mu4e-action-view-with-xwidget) t)
 
 (use-package org-caldav
   :config
